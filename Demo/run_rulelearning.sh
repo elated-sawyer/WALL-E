45789500faf7994d87837930997b0683cbd6054c
Siyu Zhou <71584437+elated-sawyer@users.noreply.github.com>
2024-10-05 14:08:47 +1000

    Delete Demo directory

--- Demo/run_rulelearning.sh
+++
@@ -1,5 +0,0 @@
-python run_rulelearning.py \
-        --model_name gpt-4 \
-        --temperature 1 \
-        --buffer /path/to/buffer.json \
-        --rule_save_dir /path/to/save/rules.json
--- Demo/utilsextra/__init__.py
+++
@@ -1,3 +0,0 @@
-from .file_utils import *
-from .json_utils import *
-from .common_utils import *
