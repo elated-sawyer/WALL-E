d6a8dfc8fb5bd89f17100258839bb4f6cd78c1fb
Siyu Zhou <71584437+elated-sawyer@users.noreply.github.com>
2024-10-05 14:11:07 +1000

    Add files via upload

---
+++ Demo/run_rulelearning.sh
@@ -0,0 +1,5 @@
+python ruleminer.py \
+        --model_name gpt-4o \
+        --temperature 0.5 \
+        --buffer buffer_fact/traj_demo.json \
+        --rule_save_dir rule_save_dir/rules_debug.json
---
+++ Demo/utilsextra/__init__.py
@@ -0,0 +1,3 @@
+from .file_utils import *
+from .json_utils import *
+from .common_utils import *
